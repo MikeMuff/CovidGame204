2c9c1946c0d3755990aa7fe5f39a8803c92d0032
Cmarquardt17 <christianmarquardt14@hotmail.com>
2020-11-09 19:36:16 -0700

    Working Doors
    I got Doors to work in the game and now going to work on the particle system for the lava and the portal

---
+++ CovidGame204/Assets/Scripts/DoorLogic.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DoorLogic : MonoBehaviour
+{
+	private IEnumerator coroutine;
+
+	private bool drawGUI = false;
+	private bool doorIsClosed = true;
+
+	public Transform theDoor;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    	if(drawGUI && Input.GetKeyDown(KeyCode.E)){
+    		changeDoorState();
+    	}
+    }
+
+    void OnTriggerEnter(Collider other) 
+    {
+        //A way of going through the "tape"
+        if (other.gameObject.CompareTag ("Player")){
+        	drawGUI = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+    	if (other.gameObject.CompareTag ("Player")){
+        	drawGUI = false;
+        }
+    }
+
+
+    void OnGUI(){
+    	if(drawGUI){
+    		GUI.Box(new Rect(Screen.width*0.5f-51,10, 102, 22), "Press E to door");
+    	}
+    }
+
+  	void changeDoorState(){
+  		Debug.Log("We got in Here");
+    	if(doorIsClosed){
+    		theDoor.GetComponent<Animation>().CrossFade("Open");
+    		theDoor.GetComponent<AudioSource>().Play();
+    		doorIsClosed = false;
+    		//yield return new WaitForSeconds(3f);
+    		coroutine = test(3.0f);
+    		StartCoroutine(coroutine);
+    		//test();
+    		doorIsClosed = true;
+
+    	}
+    }
+
+    IEnumerator test(float numSeconds){
+    	Debug.Log("We are yielding!");
+    	yield return new WaitForSeconds(numSeconds);
+    	theDoor.GetComponent<Animation>().CrossFade("Close");
+    	theDoor.GetComponent<AudioSource>().Play();
+    }
+}
