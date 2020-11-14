26475515eaa9929a28222f889715ede45dc14594
Michael Muffoletto <muffmike1@gmail.com>
2020-11-13 17:57:08 -0700

    Finised L1 Puzzle 3

---
+++ CovidGame204/Assets/Scripts/MatChanger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatChanger : MonoBehaviour
+{
+    public Material Material1;
+    //in the editor this is what you would set as the object you wan't to change
+    public GameObject Object;
+    public GameObject door;
+
+    void Start()
+    {
+        door.SetActive(false);
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        
+        Object.GetComponent<MeshRenderer>().material = Material1;
+        door.SetActive(true);
+    }
+
+
+}
