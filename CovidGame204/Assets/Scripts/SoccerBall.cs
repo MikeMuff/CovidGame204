e9954389e8effa7762074c4493414011c35bfcf6
Michael Muffoletto <muffmike1@gmail.com>
2020-11-17 22:01:00 -0700

    Finised Puzzle 4

---
+++ CovidGame204/Assets/Scripts/SoccerBall.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoccerBall : MonoBehaviour
+{
+
+    public GameObject door;
+    public AudioClip triggerSound;
+    AudioSource audioSource;
+
+    void Start()
+    {
+        door.SetActive(false);
+        audioSource = GetComponent<AudioSource>();
+    }
+
+   void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Goal")
+        {
+            
+            door.SetActive(true);
+            Debug.Log("GOALL");
+            if (triggerSound != null){
+                audioSource.PlayOneShot(triggerSound, 0.7F);
+            }
+            
+        }
+    }
+    
+    
+}
